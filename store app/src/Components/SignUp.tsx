a960c9302795de0f84e97c6b6da89a8f9ba7967b
BennyNoyman <bn0504127633@gmail.com>
2023-11-12 12:22:15 +0200

    start

---
+++ store app/src/Components/SignUp.tsx
@@ -0,0 +1,15 @@
+import {Button, TextField} from "@mui/material";
+
+const signUp = () => {
+
+}
+export default function SignUp() {
+    return(
+        <>
+            <TextField/>
+            <TextField/>
+            <TextField/>
+            <Button onClick={() => signUp()}>Sign Up</Button>
+        </>
+    )
+}
