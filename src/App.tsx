8ed920e36dd1afdec409db0f0443df13a22005c9
yehudaOhana <yehuda9955@gmail.com>
2023-11-14 13:49:46 +0200

    ProductScreen working

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
 import SignUp from "./Components/SignUp.tsx";
 import SignIn from "./Components/SignIn.tsx";
 import MainPage from "./Components/MainPage.tsx";
-import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import {NameProvider} from "./Context/loginContext.tsx";
+// import ProductScreen from "./Components/ProductScreen.tsx";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { NameProvider } from "./Context/loginContext.tsx";
 function App() {
-    return (
-        <>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<MainPage />} />
-                    <NameProvider>
-                        <Route path="/signUp" element={<SignUp />} />
-                        <Route path="SignIn" element={<SignIn/>}/>
-                    </NameProvider>
-                </Routes>
-            </Router>
-        </>
-    )
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <NameProvider>
+            <Route path="/signUp" element={<SignUp />} />
+            <Route path="SignIn" element={<SignIn />} />
+            {/* <Route path="productScreen" element={<ProductScreen />} /> */}
+          </NameProvider>
+        </Routes>
+      </Router>
+    </>
+  );
 }
-export default App
+export default App;
---
+++ src/Components/ProductScreen.tsx
@@ -0,0 +1,26 @@
+// import React from "react";
+
+// type Props = {};
+
+// async function ProductScreen({}: Props) {
+//   const id = "3";
+//   const fetchProduct = await fetch(`http://${id}`);
+//   const product = await fetchProduct.json();
+
+//   return (
+//     <div className="product-screen">
+//       <div className="product-details">
+//         <h1>{product.name}</h1>
+//         <p>{product.category}</p>
+//         <p>${product.price}</p>
+//         <p>{product.popularity}</p>
+//       </div>
+
+//       <button className="add-to-cart-btn" onClick={addToCart}>
+//         Add to Cart
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default ProductScreen;
