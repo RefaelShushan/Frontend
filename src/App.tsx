1bbe8c298c968cb56d5504700b3faa08f1fcdc5b
yehudaOhana <yehuda9955@gmail.com>
2023-11-15 09:12:58 +0200

    startday2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import SignUp from "./Components/SignUp.tsx";
 import SignIn from "./Components/SignIn.tsx";
 import MainPage from "./Components/MainPage.tsx";
+import ProductScreen from "./Components/ProductScreen.tsx";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { NameProvider } from "./Context/loginContext.tsx";
@@ -15,6 +16,7 @@ function App() {
             <Route path="/category/:category" element={<Category />} />
             <Route path="/signUp" element={<SignUp />} />
             <Route path="/signIn" element={<SignIn />} />
+            <Route path="productScreen" element={<ProductScreen />} />
           </Routes>
         </Router>
       </NameProvider>
