5b6d3e3b77a7cf1f35508a893d93c863c9330ec7
unknown <rs315744656@gmail.com>
2023-11-15 11:05:04 +0200

    home page add updated

--- src/Components/MainPage.tsx
+++ src/Components/MainPage.tsx
@@ -15,7 +15,10 @@ interface Category {
 
 export default function MainPage() {
   const [categories, setCategories] = useState<Category[] | null>(null);
+  const [categoriesPopular, setCategoriesPopular] = useState<Category[] | null>(null);
+  const [productsPopular, setProductsPopular] = useState<Category[] | null>(null);
 
+ 
   useEffect(() => {
     fetch('http://localhost:3000/api/category/')
       .then(response => {
@@ -31,6 +34,53 @@ export default function MainPage() {
       })
   }, []);
 
+  useEffect(() => {
+    fetch('http://localhost:3000/api/category/popularity ')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data: Category[]) => {
+        
+        setCategoriesPopular([...data]);
+        
+      })
+  }, []);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/products/popularity')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((dataProductPopular: Category[]) => {
+        
+        setProductsPopular([...dataProductPopular]);
+        
+      })
+  }, []);
+
+  const handleButtonClick = (categoryId: number) => {
+  
+    fetch(`http://localhost:3000/api/category/${categoryId}`, {
+      method: 'PUT',
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        
+      })
+      // .catch(error => {
+      //   console.error('Error updating popularity:', error);
+      // });
+  };
+
+
   return (
     <div>
      
@@ -39,15 +89,40 @@ export default function MainPage() {
       <h1>Main Page</h1>
       {categories  ? (
         categories.map(category => (
-          <div key={category.id}> 
+          <div>
+          <Link key={category.id} to={`/category/${category.name}`}>
             <h2>{category.name}</h2>
-            <button><img src={category.image} width={100}/></button>
+            <button onClick={() => handleButtonClick(category.id)}><img src={category.image} width={100}/></button></Link>
           </div>
         ))
       ) : (
         <p>Loading...</p>
       )}
       <h1>Category Popular</h1>
+      {categoriesPopular  ? (
+        categoriesPopular.map(categoryPopular => (
+          <div>
+          <Link key={categoryPopular.id} to={`/category/${categoryPopular.name}`}>
+            <h2>{categoryPopular.name}</h2>
+            <button><img src={categoryPopular.image} width={100}/></button></Link>
+          </div>
+        ))
+      ) : (
+        <p>Loading...</p>
+      )}
+      <h1>Products Popular</h1>
+      {productsPopular  ? (
+        productsPopular.map(productPopular => (
+          <div>
+          <Link key={productPopular.id} to={`/product/${productPopular.id}`}>
+            <button><h2>{productPopular.name}</h2></button>
+            <img src={productPopular.image} width={100}/>
+            </Link>
+          </div>
+        ))
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
